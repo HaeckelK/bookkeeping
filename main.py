33e999bc26b0403f881b41433b250d508a75f75d
HaeckelK <haeckelk.github@gmail.com>
2021-06-16 08:29:40 +0100

    refactor: move GL items to general.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from ledger import PandasLedger
 from bank import BankLedger, InMemoryBankLedger, RawBankTransaction
+from general import GLJournal, GLJournalLine, GeneralLedger
 
 
 class SourceDataLoader:
@@ -238,41 +239,6 @@ class SalesLedger(PandasLedger):
             )
             invoices.append(purchase_invoice)
         return invoices
-
-
-@dataclass
-class GLJournalLine:
-    nominal: str
-    description: str
-    amount: int
-    transaction_date: str
-
-
-@dataclass
-class GLJournal:
-    jnl_type: str
-    lines: List[GLJournalLine]
-
-
-class GeneralLedger(PandasLedger):
-    def __init__(self) -> None:
-        self.columns = ["transaction_id", "jnl_id", "nominal", "jnl_type", "amount", "description", "transaction_date"]
-        self.df = pd.DataFrame(columns=self.columns)
-        return
-
-    def get_next_journal_id(self) -> int:
-        try:
-            next_id = int(self.df["jnl_id"].max()) + 1
-        except ValueError:
-            return 0
-        return next_id
-
-    def add_journal(self, journal: GLJournal) -> None:
-        df = pd.DataFrame([asdict(x) for x in journal.lines])
-        df["jnl_type"] = journal.jnl_type
-        df["jnl_id"] = self.get_next_journal_id()
-        self.append(df)
-        return
 
 
 class InterLedgerJournalCreator:
