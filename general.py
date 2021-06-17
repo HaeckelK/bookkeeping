93de451cfdbb95ba144ccd5e91f6e7f23a4675a5
HaeckelK <haeckelk.github@gmail.com>
2021-06-17 22:48:17 +0100

    feat: checks for gl total. pl-gl. sl-gl

--- general.py
+++ general.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-from typing import List
+from typing import List, Dict
 
 import pandas as pd
 
@@ -56,6 +56,9 @@ class GeneralLedger(PandasLedger):
 
     @property
     def balance(self) -> int:
-        return sum(x.amount for x in self.list_transactions())
+        return self.df["amount"].sum()
 
-    # TODO balances / trial balance
+    @property
+    def balances(self) -> Dict[str, int]:
+        data = self.df[['nominal', 'amount']].groupby(['nominal']).sum().to_dict()["amount"]
+        return data
