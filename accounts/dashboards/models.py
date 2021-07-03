b488d6a0e68bc88a4857957213debd5219a6c1f2
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-07-03 18:11:50 +0100

    Front end (#32)
    * feat: draft example of Trial Balance report
    
    * feat: add expected_sign into NominalAccount
    
    * feat: is_control_account is_bank_account
    
    * feat: draft example of Trial Balance report
    
    * feat: add expected_sign into NominalAccount
    
    * feat: is_control_account is_bank_account
    
    * build: temporary end to end script
    
    * feat: dummy period balances data
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

---
+++ accounts/dashboards/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class NominalAccount(models.Model):
+    EXPECTED_SIGN_CHOICES = [
+        ('dr', 'debit'),
+        ('cr', 'credit'),
+    ]
+    name = models.CharField(max_length=100)
+    expected_sign = models.CharField(
+        max_length=2,
+        choices=EXPECTED_SIGN_CHOICES
+    )
+    is_control_account = models.BooleanField()
+    is_bank_account = models.BooleanField()
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class JournalLine(models.Model):
+    nominal = models.ForeignKey(NominalAccount, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.amount} - {self.nominal}"
+
+
+class Journal(models.Model):
+    description = models.CharField(max_length=250)
+    period = models.IntegerField()
+    # date_posted
+    lines = models.ManyToManyField(JournalLine)
+
+
+class GLTransactionLine(models.Model):
+    pass
+
+
+class PeriodBalance(models.Model):
+    nominal = models.ForeignKey(NominalAccount, on_delete=models.CASCADE)
+    period = models.IntegerField()
+    amount = models.IntegerField()
+    amount_cumulative = models.IntegerField()
+    count_transactions = models.IntegerField()
