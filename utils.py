3ac2d8e84900c8b090e0779e0046ba17e802bb05
HaeckelK <haeckelk.github@gmail.com>
2021-07-08 08:40:46 +0100

    fix: remove print statements

--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
 def convert_date_string_to_period(timestamp) -> int:
-    print("entered here", timestamp)
     try:
         month = int(timestamp.month)
     except AttributeError:
-        print("went wrong here")
         return -1
     else:
         return month
